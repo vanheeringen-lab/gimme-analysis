a31d7bbbb71d72ed31d36621c34048a05fcd23c2
simonvh <simon.vanheeringen@gmail.com>
2019-06-03 13:21:22 +0200

    use gzip instead of pigz

--- motif_database_comparison/scripts/download_remap_peaks.sh
+++ motif_database_comparison/scripts/download_remap_peaks.sh
@@ -14,7 +14,7 @@ fi
 zcat $DATA_DIR/$REMAP_FILE | awk -vd=$DATA_DIR '{print $1 "\t" $7 - 50 "\t"  $8 + 50 >> d"/"$4; close($4)}'; for i in `ls $DATA_DIR/[A-Z]* | grep -v bed`; do mv $i $i.bed; done
 
 # gzip it!
-pigz $DATA_DIR/*.bed
+gzip $DATA_DIR/*.bed
 
 # Remove all files with less than 1000 peaks
 for i in $DATA_DIR/[A-Z]*bed.gz; do echo $i `zcat $i | wc -l`; done | tr \  \\t | sort -k2g | awk -vnpeak=$MIN_NPEAK '$2 < npeak' | cut -f1 | xargs rm
